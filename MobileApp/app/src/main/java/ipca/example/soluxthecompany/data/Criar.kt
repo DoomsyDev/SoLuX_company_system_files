e40b89d8e3e5548d7dae74fa4b867f1f9ec7bc04
Rúben Barbosa <ru.borgs@gmail.com>
2025-10-29 08:39:42 +0000

    data class changed

---
+++ MobileApp/app/src/main/java/ipca/example/soluxthecompany/data/Criar.kt
@@ -0,0 +1,13 @@
+package ipca.example.soluxthecompany.data
+
+class Criar {
+    data class TerrenoState(
+        val nome: String = "",
+        val dataImplementacao: String = "",
+        val dataLevantamento: String = "",
+        val latitude: String = "",
+        val longitude: String = "",
+        val device: String = "",
+        val sampling: String = ""
+    )
+}
--- MobileApp/app/src/main/java/ipca/example/soluxthecompany/itui/CriarViewModel.kt
+++ MobileApp/app/src/main/java/ipca/example/soluxthecompany/itui/CriarViewModel.kt
@@ -4,21 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import ipca.example.soluxthecompany.data.Criar
 
 // Data class para guardar as informações do formulário
-data class TerrenoState(
-    val nome: String = "",
-    val dataImplementacao: String = "",
-    val dataLevantamento: String = "",
-    val latitude: String = "",
-    val longitude: String = "",
-    val device: String = "",
-    val sampling: String = ""
-)
 
 class CriarViewModel : ViewModel() {
 
-    var terrenoState by mutableStateOf(TerrenoState())
+    var terrenoState by mutableStateOf(Criar.TerrenoState())
         private set
 
     // Funções para atualizar cada campo individualmente
