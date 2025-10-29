f441f9e9541a580a3d63c94f17c515c0f5f79a12
Rúben Barbosa <ru.borgs@gmail.com>
2025-10-29 07:05:18 +0000

    changed name main page

--- MobileApp/app/src/main/java/ipca/example/soluxthecompany/itui/HomeScreen.kt
+++ MobileApp/app/src/main/java/ipca/example/soluxthecompany/itui/HomeScreen.kt
@@ -56,7 +56,7 @@ fun HomeScreen(
             TopAppBar(
                 title = {
                     Text(
-                        "SoLuX",
+                        "SoLuX The Startup",
                         color = Color.White,
                         fontSize = 20.sp
                     )
